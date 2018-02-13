e6bf8aade4f817829f0ee0b12a915b12a635029e
anderson.botega <anderson.botega@sabium.com.br>
2018-02-13 14:38:06 -0200

    rest ligados

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,10 @@
-var urlServidor = "http://localhost/php/ai/";
+var urlServidor = "http://localhost/php/ai";
 
 function validar() {
 
     loadingElement('btnValidarLogin', 'Validando...')
 
-    var url = urlServidor + "admin/logar";
+    var url = urlServidor + "/admin/logar";
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
@@ -30,77 +30,84 @@ function validar() {
 
 }
 
-// function esqueceuSenha() {
-//     openPage('esqueceuSenha.html', {chave: 'valor'}, loadPageEsqueceuSenha)
-// }
+function listarTotais() {
+    var url = urlServidor + "/total";
+    var loginServidor = JSON.parse(localStorage.getItem('login'));
 
-// function cadastreSe() {
-// openPage('cadastreSe.html')
-// }
+    MobileUI.ajax
+        .get(url)
+        .set('token', loginServidor['token'])
+        .set('Content-Type', 'application/json')
+        .end(function (error, res) {
 
-// function loadPageEsqueceuSenha(params) {
-// console.log(params);
-// }
+            var ret = JSON.parse(res['text']);
+
+            if (error && res['status'] !== 200) {
+
+                if (res['status'] == 403) {
+                    openPage('index')
+                    return false;
+                }
+
+                alert(ret['error']);
+                return false;
+            }
+
+            document.getElementById('totalCadastros').innerHTML = ret['retorno'].cadastros;
+            document.getElementById('totalContratos').innerHTML = ret['retorno'].contratos;
+            document.getElementById('totalMensalidades').innerHTML = ret['retorno'].mensalidades;
+        });
+}
 
 document.addEventListener('openPage', function (e) {
-    //It does something when the page is opened
-    //You can access name of page in: e.detail.page
+    var pageAtual = e.detail.page;
+
+    if (pageAtual == "principal.html") {
+        listarTotais();
+        loadPrincipal();
+    }
+
+    if (pageAtual == "cadastros.html") {
+        listarCadastros();
+    }
+
+    if (pageAtual == "contratos.html") {
+        listarContratos();
+    }
+
+    if (pageAtual == "mensalidades.html") {
+        listarMensalidades();
+    }
 })
 
-function editCadastro(params) {
-    // openPage('editCadastro', {name:'Jeanette Fletcher'}, functionOpenContact)
-    openPage('editCadastro')
+MobileUI.recorteString = function (params, qtd) {
+    if (params.length > qtd) {
+        return params.substr(0, qtd) + "...";
+    }
+
+    return params;
 }
 
-function editContrato(params) {
-    // openPage('editCadastro', {name:'Jeanette Fletcher'}, functionOpenContact)
-    openPage('editContrato')
+MobileUI.situacao = function (id_situacao) {
+    if (id_situacao == 1) {
+        return 'Em aberto';
+    }
+    if (id_situacao == 2) {
+        return 'Finalizado';
+    }
+    if (id_situacao == 3) {
+        return 'Cancelado';
+    }
 }
 
-function updateValuesProgressSemicircle() {
-    document.getElementById('progressMetricUser').progressSemicircle.update(Math.floor(Math.random() * 100) + 1);
-}
-
-function updateValuesProgressCircle() {
-    document.getElementById('progressProfile').progressCircle.update(Math.floor(Math.random() * 100) + 1);
-    document.getElementById('progressSteps').progressCircle.update(Math.floor(Math.random() * 100) + 1);
-    document.getElementById('progressRunning').progressCircle.update(Math.floor(Math.random() * 100) + 1);
-    document.getElementById('progressTasks').progressCircle.update(Math.floor(Math.random() * 100) + 1);
-}
-
-function salvarContrato() {
-    loadingElement('btnSalvarContrato', 'Salvando...')
-
-    alert('Registro salvo com sucesso')
-
-    closeLoading('btnSalvarContrato')
-}
-
-function darBaixa() {
-    alert({
-        title: 'Baixa de mensalidade',
-        template: 'template-alert-custom',
-        width: '90%',
-        buttons: [
-            {
-                label: 'Salvar',
-                onclick: salvarBaixa
-            },
-            {
-                label: 'Cancelar'
-            }
-        ]
-    });
-}
-
-function salvarBaixa() {
-    console.log('to aqui')
-}
-
-function salvarCadastro() {
-    loadingElement('btnSalvarCadastro', 'Salvando...')
-
-    alert('Registro salvo com sucesso')
-
-    closeLoading('btnSalvarCadastro')
-}
+MobileUI.statusSituacao = function (id_situacao) {
+    if (id_situacao == 1) {
+        return '<i class="icon ion-android-happy text-grey"></i>';
+    }
+    if (id_situacao == 2) {
+        return '<i class="icon ion-android-happy text-green"></i>';
+    }
+    if (id_situacao == 3) {
+        return '<i class="icon ion-android-sad text-red"></i>';
+    }
+}	
